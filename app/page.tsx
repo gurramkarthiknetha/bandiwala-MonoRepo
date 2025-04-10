ddcb882460804331da856116accf1ff4ca241668
Manojkumar132006 <63360464+Manojkumar132006@users.noreply.github.com>
2025-04-10 19:17:20 +0530

    Create web

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,26 @@
+import React from 'react';
+import Header from '@/components/Header';
+import Hero from '@/components/Hero';
+import ServiceButtons from '@/components/ServiceButtons';
+import FeaturedVendors from '@/components/FeaturedVendors';
+import TrendingDishes from '@/components/TrendingDishes';
+import Reviews from '@/components/Reviews';
+import MapSection from '@/components/MapSection';
+import CTA from '@/components/CTA';
+import Footer from '@/components/Footer';
+
 export default function Home() {
   return (
-    <main>
-      <div>Hello world!</div>
-    </main>
+    <div className="min-h-screen flex flex-col">
+      <Header />
+      <Hero />
+      <ServiceButtons />
+      <FeaturedVendors />
+      <TrendingDishes />
+      <Reviews />
+      <MapSection />
+      <CTA />
+      <Footer />
+    </div>
   );
 }
